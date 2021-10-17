63c7292e12bd1e9ea3cf2e0a80f8d22423ab9ff0
guidogacz <guidosistemas@gmail.com>
2021-10-17 08:19:45 -0500

    se borran lineas q presentan conflicto en git, no afecta el modulo de usuarios

--- SPRINT 3/backend/server.js
+++ SPRINT 3/backend/server.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 import { conectarBD } from './db/dbUsuarios.js';
 import rutaUsuarios from './views/rutaUsuarios.js';
 
+
 dotenv.config({ path: './.env' });
 const app = Express();
 app.use(Express.json());
 app.use(Cors());
 app.use(rutaUsuarios);
+
 
 const main = () => {
     return app.listen(process.env.PORT, () => {
