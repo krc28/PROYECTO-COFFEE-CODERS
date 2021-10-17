96a596a59e0ea3fb0747eb415208a7e9c3c4a59f
krc28 <restreka28@gmail.com>
2021-10-17 18:05:58 -0500

    actualizado

--- proyecto/src/components/IconoVentas.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Icono from 'media/IconoVentas.png';
-
-const IconoVentas = () => {
-  return <img className='rounded-full flex items-center justify-center mx-auto h-40 w-auto' src={Icono} alt='Workflow' />;
-};
-
-export default IconoVentas;
