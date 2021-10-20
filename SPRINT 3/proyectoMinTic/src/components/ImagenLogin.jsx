0cc563eb80eb01fe5fadc037feb1475f9cff6eff
guidogacz <guidosistemas@gmail.com>
2021-10-20 09:35:42 -0500

    nuevo logocoffeecoders en login

--- SPRINT 3/proyectoMinTic/src/components/ImagenLogin.jsx
+++ SPRINT 3/proyectoMinTic/src/components/ImagenLogin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from 'media/cafemolido.jpg';
+import Logo from 'media/logocoffeecoders.png';
 
 const ImagenLogin = () => {
   return <img className='rounded-full flex items-center justify-center mx-auto h-40 w-auto' src={Logo} alt='Workflow' />;
