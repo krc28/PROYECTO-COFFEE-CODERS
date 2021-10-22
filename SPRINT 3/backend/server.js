9eb6488a878cc578d7c270bd503b6d797fd4f774
guidogacz <guidosistemas@gmail.com>
2021-10-22 16:52:05 -0500

    se añade nodemon

--- SPRINT 3/backend/server.js
+++ SPRINT 3/backend/server.js
@@ -5,25 +5,25 @@ import { conectarBD } from './db/dbMongo.js';
 import rutaUsuarios from './views/rutaUsuarios.js';
 import rutaVentas from './views/rutaVentas.js';
 import rutaProductos from './views/rutaProductos.js';
-import jwt from 'express-jwt';
-import jwks from 'jwks-rsa';
+// import jwt from 'express-jwt';
+// import jwks from 'jwks-rsa';
 
 dotenv.config({ path: './.env' });
 const app = Express();
 app.use(Express.json());
 app.use(Cors());
 
-var jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: 'https://coffeecoders-mintic.us.auth0.com/.well-known/jwks.json'
-}),
-audience: 'api-autenticacion-coffeecoders',
-issuer: 'https://coffeecoders-mintic.us.auth0.com/',
-algorithms: ['RS256']
-});
+// var jwtCheck = jwt({
+//   secret: jwks.expressJwtSecret({
+//       cache: true,
+//       rateLimit: true,
+//       jwksRequestsPerMinute: 5,
+//       jwksUri: 'https://coffeecoders-mintic.us.auth0.com/.well-known/jwks.json'
+// }),
+// audience: 'api-autenticacion-coffeecoders',
+// issuer: 'https://coffeecoders-mintic.us.auth0.com/',
+// algorithms: ['RS256']
+// });
 
 app.use(jwtCheck);
 app.use(rutaUsuarios);
